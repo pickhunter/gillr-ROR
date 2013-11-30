1cb876ada3e344198a1e6d55727b09c4dd6f988f
Rohit Garg <rohhittt@gmail.com>
2013-11-30 21:11:28 +0530

    App Generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gillr::Application.config.secret_key_base = 'deb784d3fe6dac15c642feace40131e8978dc5d9e50eaa39922c968e2c481e6caf6ae9a0e03903e92b8ade5e2a0431b98212ea0f9eca136b9283c197f6381fd9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gillr::Application.config.session_store :cookie_store, key: '_gillr_session'
